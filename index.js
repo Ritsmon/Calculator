6454bf18e4cdc973344443b17e7e0a1baf29c69c
Richmond Gupo <richmondgupo45@gmail.com>
2026-01-05 18:20:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const display = document.getElementById('display');
 function appendToDisplay(input){
     display.value += input;
 }
-
 function clearDisplay(){
     display.value = "";
 }
-
 function calculate(){
     try{
         display.value = eval(display.value);
-    }catch{
+    }
+    catch(e){
         display.value = "ERROR";
     }
+
 }
